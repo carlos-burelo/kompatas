0ef1c78c928d97d7f686599a83f455d9285128bb
carlos-burelo <carlosfernandoburelo@gmail.com>
2023-12-03 09:12:40 -0600

    fix pwa resources

--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -5,17 +5,17 @@ import type { ManifestOptions } from "vite-plugin-pwa"
  * Defines the default SEO configuration for the website.
  */
 export const seoConfig = {
-	baseURL: "https://example.com", // Change this to your production URL.
+	baseURL: "https://kompatas.vercel.app", // Change this to your production URL.
 	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to be your website's description.
+		"Kompatas is an opionated Astro starter for building robust static websites.", // Change this to be your website's description.
 	type: "website",
 	image: {
-		url: "https://picsum.photos/1200/630", // Change this to your website's thumbnail.
+		url: "https://kompatas.vercel.app/logo.png", // Change this to your website's thumbnail.
 		alt: "OpenGraph thumbnail description.", // Change this to your website's thumbnail description.
 		width: 1200,
 		height: 630
 	},
-	siteName: "Astro PWA Starter", // Change this to your website's name,
+	siteName: "Kompatas", // Change this to your website's name,
 	twitter: {
 		card: "summary_large_image"
 	}
@@ -25,11 +25,11 @@ export const seoConfig = {
  * Defines the configuration for PWA webmanifest.
  */
 export const manifest: Partial<ManifestOptions> = {
-	name: "Astro PWA Starter", // Change this to your website's name.
-	short_name: "Astro PWA Starter", // Change this to your website's short name.
+	name: "Kompatas", // Change this to your website's name.
+	short_name: "Kompatas", // Change this to your website's short name.
 	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to your websites description.
-	theme_color: "#30E130", // Change this to your primary color.
+		"Kompatas is an opionated Astro starter for building robust static websites.", // Change this to your websites description.
+	theme_color: "#ffffff", // Change this to your primary color.
 	background_color: "#ffffff", // Change this to your background color.
 	display: "minimal-ui",
 	icons: [
