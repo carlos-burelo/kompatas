d01ae0644f640738cb5f3e1a901245f9d3f8d44a
carlos-burelo <carlosfernandoburelo@gmail.com>
2023-12-03 09:36:30 -0600

    fix navigation and create new pages

--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -31,7 +31,7 @@ export const manifest: Partial<ManifestOptions> = {
 		"Kompatas is an opionated Astro starter for building robust static websites.", // Change this to your websites description.
 	theme_color: "#ffffff", // Change this to your primary color.
 	background_color: "#ffffff", // Change this to your background color.
-	display: "minimal-ui",
+	display: "fullscreen",
 	icons: [
 		{
 			src: "/favicons/favicon-192x192.png",
